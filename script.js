698c5718a1ba5bc1819e578f83ac94a379a542f4
JoacoMaurtua <joacomaurtua@gmail.com>
2021-04-18 22:46:21 -0500

    75%

---
+++ script.js
@@ -0,0 +1,83 @@
+/******exercise 1*****/ 
+
+/*function a(){
+  return 55;
+}
+console.log(a())
+
+/******exercise 2*****/ 
+/*function a(){
+  return 4;
+}
+console.log(a()+a());
+
+/******exercise 3*****/ 
+/*function a(b){
+  return b;
+}
+
+console.log(a(2)+a(4));
+
+/******exercise 4*****/ 
+/*function a(b){
+  console.log(b);
+  return b*3;
+}
+console.log(a(3));
+
+/******exercise 5*****/ 
+/*function a(b){
+  return b*4;
+  console.log(b); //ignore becouse return
+}
+console.log(a(10));
+
+/******exercise 6*****/ 
+/*function a(b){
+  if(b<10) {
+      return 2;
+  }
+  else     {
+      return 4;
+  }
+  console.log(b);
+}
+console.log(a(15));
+
+/******exercise 7*****/ 
+/*function a(b,c){
+  return b*c;
+}
+console.log(10,3);
+console.log( a(3,10) );
+
+/******exercise 8*****/ 
+/*function a(){
+  for(var i=0; i<10; i++){
+      i = i +2;
+      console.log(i);
+  }
+}
+a();
+
+/******exercise 9*****/ 
+/*function a(b,c){
+  for(var i=b; i<c; i++) {
+     console.log(i);
+ }
+ return b*c;
+}
+a(0,10);
+console.log(a(0,10));
+
+/******exercise 10*****/ 
+function a(){
+  for(var i=0; i<10; i++){
+     for(var j=0; j<10; j++){
+         console.log(j);
+     }
+     console.log(i);
+  }
+}
+
+a();
